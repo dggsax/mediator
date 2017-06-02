c3117d3bbc34aad2a31c65bf42fea70acecf169e
Gonzo <dggsax@gmail.com>
2017-06-02 14:28:26 -0400

    Working v1.0

--- mediator.h
+++ mediator.h
@@ -5,28 +5,43 @@
 
 #ifndef mediator
 #define mediator
-// #endif                                                               
+
+// Define the global variables for values that will be communicated back and forth between teensy and website
+extern float Kp;
+extern float Kd;
+extern float Kbemf;
+extern float Ku;
+extern float Ki;
+extern float directV;
+extern float desiredV;
+
 
 // include types & constants of Wiring core API
+#include <WString.h>
 #include "WConstants.h"
 
 // library interface description
 class MEDIATOR {
-  // user-accessible "public" interface
-  public:
-  	MEDIATOR();
-    MEDIATOR(int baud);
-    // Test(int);
-    // void flag(void);
-    // void pack(void);
-    void pack();
+	// user-accessible "public" interface
+	public:
+  		MEDIATOR();
+    	MEDIATOR(int baud);
+    	// Test(int);
+    	// void flag(void);
+    	// void pack(void);
+    	void setup(int deltaT, int transferDt);
+    	void pack(float a, float b, float c, float d);
+    	void restart(String config_message);
 
-  // library-accessible "private" interface
-  private:
-  	// Make MEDIATOR a static member of the class (apparently necessary)
-  	static MEDIATOR * _instance;
+  	// library-accessible "private" interface
+  	private:
+  		// Make MEDIATOR a static member of the class (apparently necessary)
+  		static MEDIATOR * _instance;
 
-    void init(int baud);
+	    void init(int baud);
+	    void initloop();
+	    void processString(String inputString);
+	    void serialEvent();
 };
 
 #endif
